c15d08b79089f24c20e9c6fa7a738ebf43c1e9f5
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-10-13 21:50:48 +0300

    add data location widget

--- lib/view/widgets/location_widget.dart
+++ lib/view/widgets/location_widget.dart
@@ -2,10 +2,14 @@ import 'package:flutter/widgets.dart';
 import 'package:sky_cast/core/themes/app_styles.dart';
 import 'package:sky_cast/models/current_weather_model.dart';
 import 'package:sky_cast/models/forecast_model.dart';
+import 'package:sky_cast/util/helpers/app_helper.dart';
 
 class LocationWidget extends StatelessWidget {
-  const LocationWidget(
-      {super.key, required this.currentWeatherModel, required this.day});
+  const LocationWidget({
+    super.key,
+    required this.currentWeatherModel,
+    required this.day,
+  });
   final CurrentWeatherModel currentWeatherModel;
   final Day day;
 
@@ -20,25 +24,28 @@ class LocationWidget extends StatelessWidget {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                currentWeatherModel.location!.name!,
-                style: AppStyles.bodySemiBoldLarge,
-              ),
-              Text(
-                currentWeatherModel.location!.country!,
-                style: AppStyles.bodyRegularSmall,
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                currentWeatherModel.location!.localtime!,
-                style: AppStyles.bodyRegularSmall,
-              ),
-            ],
+          Expanded(
+            flex: 4,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  currentWeatherModel.location!.name!,
+                  style: AppStyles.bodySemiBoldLarge,
+                ),
+                Text(
+                  currentWeatherModel.location!.country!,
+                  style: AppStyles.bodyRegularSmall,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  '${AppHelper.getHumanReadableData(currentWeatherModel.location!.localtime!.split(' ').first)}, ${AppHelper.getHumanReadableData(currentWeatherModel.location!.localtime!.split(' ').last)}',
+                  style: AppStyles.bodyRegularSmall,
+                ),
+              ],
+            ),
           ),
           const Spacer(),
           Image.network(
