e60dd1c8caed30d5e09bfc7495a750fdd0406594
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-10-13 23:39:21 +0300

    add data to home view

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -3,12 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
+import 'package:path/path.dart';
 import 'package:sky_cast/controller/weather_controller.dart';
+import 'package:sky_cast/util/helpers/app_helper.dart';
 import 'package:sky_cast/view/widgets/current_weather.dart';
 import 'package:sky_cast/view/widgets/custom_darwer.dart';
 import 'package:sky_cast/view/widgets/daily_forecast.dart';
+import 'package:sky_cast/view/widgets/double_property_widget.dart';
 import 'package:sky_cast/view/widgets/hourly_forecast.dart';
 import 'package:sky_cast/view/widgets/outlook_widget.dart';
+import 'package:sky_cast/view/widgets/property_widget.dart';
 
 class HomeView extends StatelessWidget {
   HomeView({super.key});
@@ -55,11 +59,77 @@ class HomeView extends StatelessWidget {
                                 const SizedBox(
                                   height: 10,
                                 ),
-                                const OutlookWidget(),
+                                OutlookWidget(
+                                  controller: controller,
+                                  astro: controller.forecastModel!.forecast!
+                                      .forecastday!.first.astro!,
+                                ),
                                 const SizedBox(
                                   height: 10,
                                 ),
-                                DailyForecast(),
+                                DailyForecast(
+                                  forecastday: controller
+                                      .forecastModel!.forecast!.forecastday!,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Row(
+                                  children: [
+                                    PropertyWidget(
+                                      name: 'UV index',
+                                      value: AppHelper.getUVIndexDescription(
+                                        controller.currentWeather!.current!.uv!,
+                                      ),
+                                      photo: 'uv.svg',
+                                    ),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    PropertyWidget(
+                                      name: 'Humidity',
+                                      value:
+                                          "${controller.currentWeather!.current!.humidity}%",
+                                      photo: 'water.svg',
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Row(
+                                  children: [
+                                    PropertyWidget(
+                                      name: 'Wind',
+                                      value:
+                                          "${controller.currentWeather!.current!.windKph!.toInt()} km/h",
+                                      photo: 'wind.svg',
+                                    ),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    DoublePropertyWidget(
+                                      photo1: 'sunrise.svg',
+                                      photo2: 'sunset.svg',
+                                      name1: 'Sunrise',
+                                      name2: 'Sunset',
+                                      value1: controller
+                                          .forecastModel!
+                                          .forecast!
+                                          .forecastday!
+                                          .first
+                                          .astro!
+                                          .sunrise!,
+                                      value2: controller
+                                          .forecastModel!
+                                          .forecast!
+                                          .forecastday!
+                                          .first
+                                          .astro!
+                                          .sunset!,
+                                    ),
+                                  ],
+                                ),
                               ],
                             ),
                           ),
