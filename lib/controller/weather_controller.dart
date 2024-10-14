0fceb9124d266c8a1096b88e6b9152f0a1ba3799
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-10-14 15:11:10 +0300

    add reorder & search functions

--- lib/controller/weather_controller.dart
+++ lib/controller/weather_controller.dart
@@ -1,14 +1,18 @@
 import 'dart:async';
 
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:sky_cast/controller/settings_controller.dart';
+import 'package:sky_cast/core/config/app_routes.dart';
 import 'package:sky_cast/core/themes/app_themes.dart';
 import 'package:sky_cast/models/api_response.dart';
 import 'package:sky_cast/models/current_weather_model.dart';
 import 'package:sky_cast/models/forecast_model.dart';
+import 'package:sky_cast/models/search_location.dart';
+import 'package:sky_cast/models/weather.dart';
 import 'package:sky_cast/util/services/api_service.dart';
 
 class WeatherController extends GetxController {
@@ -22,8 +26,11 @@ class WeatherController extends GetxController {
   int currentOutlookPage = 0;
   Color? backgroundColor;
   late ApiResponse responseState;
+  Rx<ApiResponse> searchState = ApiResponse.unknownError.obs;
   SettingsController settingsController = Get.find<SettingsController>();
   Timer? timer;
+  RxList<Weather> weathers = <Weather>[].obs;
+  RxList<SearchLocation> searchLocations = <SearchLocation>[].obs;
 
   Future<void> getWeatherData(String location) async {
     isLoading(true);
@@ -34,6 +41,8 @@ class WeatherController extends GetxController {
       Map<String, dynamic> jsonResponse = response.getOrElse(() => {});
       currentWeather = CurrentWeatherModel.fromJson(jsonResponse);
       forecastModel = ForecastModel.fromJson(jsonResponse);
+      weathers.add(Weather(
+          currentWeatherModel: currentWeather!, forecastModel: forecastModel!));
       box.write('location', location);
       this.location = location;
       updateBackgroundColor();
@@ -41,6 +50,31 @@ class WeatherController extends GetxController {
       responseState = response.fold((l) => l, (r) => ApiResponse.unknownError);
     }
     isLoading(false);
+    update();
+  }
+
+  void searchLocation(String query) async {
+    searchState(ApiResponse.loading);
+    var response =
+        await apiService.getRequst(endPoint: '&q=$query', isSearch: true);
+    response.fold((left) {
+      searchState.value = left;
+    }, (right) {
+      List<dynamic> jsonRes = right;
+      if (jsonRes.isNotEmpty) {
+        searchState(ApiResponse.ok);
+        searchLocations.value = jsonRes
+            .map(
+                (json) => SearchLocation.fromJson(json as Map<String, dynamic>))
+            .toList();
+      }
+    });
+  }
+
+  void selectLocation(String loc) {
+    getWeatherData(loc);
+    searchLocations.clear();
+    Get.toNamed(AppRoutes.home);
     update();
   }
 
@@ -65,6 +99,13 @@ class WeatherController extends GetxController {
     if (settingsController.refreshTime.inHours != 0) {
       refreshWeather();
     }
+  }
+
+  void onReorder(int oldIndex, int newIndex) {
+    if (oldIndex < newIndex) newIndex--;
+    final Weather tempWeather = weathers.removeAt(oldIndex);
+    weathers.insert(newIndex, tempWeather);
+    update();
   }
 
   @override
--- lib/view/widgets/search/search_result_widget.dart
+++ lib/view/widgets/search/search_result_widget.dart
@@ -1,10 +1,53 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sky_cast/controller/weather_controller.dart';
+import 'package:sky_cast/core/themes/app_styles.dart';
+import 'package:sky_cast/models/search_location.dart';
 
 class SearchResultWidget extends StatelessWidget {
-  const SearchResultWidget({super.key});
+  const SearchResultWidget(
+      {super.key, required this.controller, required this.searchedLocation});
+  final WeatherController controller;
+  final RxList<SearchLocation> searchedLocation;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 13,
+      ),
+      decoration: BoxDecoration(
+        color: const Color(0xFF212121),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: ListView.builder(
+        itemCount: searchedLocation.length,
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        itemBuilder: (context, index) => InkWell(
+          onTap: () => controller.selectLocation(searchedLocation[index].name!),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                searchedLocation[index].name!,
+                style: AppStyles.bodySemiBoldLarge,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                "${searchedLocation[index].region!} ${searchedLocation[index].country!}",
+              ),
+              if (index != searchedLocation.length - 1)
+                const Divider(
+                  height: 20,
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
