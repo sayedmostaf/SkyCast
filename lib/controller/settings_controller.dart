592ab2317a7ad4b3608e37a37e7ec43e12709ba1
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-10-14 11:39:55 +0300

    create settings functionallity

--- lib/controller/settings_controller.dart
+++ lib/controller/settings_controller.dart
@@ -1,5 +1,38 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class SettingsController extends GetxController {
   final RxBool isDarkTheme = true.obs;
+  GetStorage box = GetStorage();
+  String unit = 'C';
+  Duration refreshTime = const Duration(hours: 1);
+
+  void setUnit(String unit) {
+    if (unit == 'C') {
+      this.unit = 'C';
+    } else {
+      this.unit = 'F';
+    }
+    box.write('unit', unit);
+    update();
+  }
+
+  void setRefreshTime(int hours) {
+    refreshTime = Duration(hours: hours);
+    box.write('refreshTime', hours);
+    update();
+  }
+
+  void updateDate() {
+    unit = box.read('unit') ?? unit;
+    int? storedHours = box.read('refreshTime');
+    refreshTime =
+        storedHours != null ? Duration(hours: storedHours) : refreshTime;
+  }
+
+  @override
+  void onInit() {
+    updateDate();
+    super.onInit();
+  }
 }
