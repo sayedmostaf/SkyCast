a912b379c5093e55ba08914296793707ec8b38b0
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-10-14 15:22:02 +0300

    create search view

--- lib/core/config/app_routes.dart
+++ lib/core/config/app_routes.dart
@@ -1,12 +1,14 @@
 import 'package:get/get.dart';
 import 'package:sky_cast/view/home_view.dart';
 import 'package:sky_cast/view/manage_location_view.dart';
+import 'package:sky_cast/view/search_view.dart';
 import 'package:sky_cast/view/settings_view.dart';
 
 class AppRoutes {
   static const String home = '/';
   static const String locations = '/locations';
   static const String settings = '/settings';
+  static const String search = '/search';
   static List<GetPage> routes = [
     GetPage(
       name: home,
@@ -19,6 +21,10 @@ class AppRoutes {
     GetPage(
       name: settings,
       page: () => const SettingsView(),
+    ),
+    GetPage(
+      name: search,
+      page: () => const SearchView(),
     ),
   ];
 }
---
+++ lib/view/search_view.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+import 'package:sky_cast/controller/weather_controller.dart';
+import 'package:sky_cast/models/api_response.dart';
+import 'package:sky_cast/view/widgets/search/search_result_widget.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search location'),
+      ),
+      body: GetBuilder<WeatherController>(
+        builder: (controller) => SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SearchBar(
+                    padding: const WidgetStatePropertyAll(
+                      EdgeInsets.symmetric(horizontal: 15),
+                    ),
+                    elevation: const WidgetStatePropertyAll(1),
+                    hintText: 'Search location..',
+                    trailing: const [Icon(Icons.search)],
+                    onChanged: (value) => controller.searchLocation(value),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Obx(() {
+                    if (controller.searchState.value == ApiResponse.ok) {
+                      return SearchResultWidget(
+                        controller: controller,
+                        searchedLocation: controller.searchLocations,
+                      );
+                    } else if (controller.searchState.value ==
+                        ApiResponse.offline) {
+                      return Lottie.asset('assets/lotties/offline.json');
+                    } else if (controller.searchState.value ==
+                        ApiResponse.loading) {
+                      return Lottie.asset('assets/lotties/loading.json');
+                    } else {
+                      return Lottie.asset('assets/lotties/search.json');
+                    }
+                  })
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
